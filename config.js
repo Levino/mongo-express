ebc2c7e44b15256c34919454affddfa513275208
Levin Keller <levin@coyno.com>
2016-01-04 13:08:27 +0100

    Renaming env vars to proper names.

--- config.js
+++ config.js
@@ -18,12 +18,12 @@ module.exports = {
         //set admin to true if you want to turn on admin features
         //if admin is true, the auth list below will be ignored
         //if admin is true, you will need to enter an admin username/password below (if it is needed)
-        admin: (process.env.ME_CONFIG_ADMIN_MODE === "true"),
+        admin: (process.env.ME_CONFIG_MONGODB_ADMIN_MODE === "true"),
 
 
         // >>>>  If you are using regular accounts, fill out auth details in the section below
         // >>>>  If you have admin auth, leave this section empty and skip to the next section
-        auth: JSON.parse(process.env.ME_CONFIG_AUTH),
+        auth: JSON.parse(process.env.ME_CONFIG_MONGODB_AUTH),
 
 
         //  >>>>  If you are using an admin mongodb account, or no admin account exists, fill out section below
